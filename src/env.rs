4e6697c957ddda0084c9d58c4acc22d6e4ab48e8
cykreet <cykreet@gmail.com>
2022-07-08 23:16:59 +0200

    fix: enforce embed limit

--- src/env.rs
+++ src/env.rs
@@ -3,8 +3,7 @@ use std::{env, str::FromStr, time::Duration};
 pub const DEFAULT_PORT: u16 = 8080;
 pub const DEFAULT_DELIVER_DURATION: Duration = Duration::from_secs(7);
 pub const DEFAULT_WEBHOOK_ENDPOINT: &str = "https://discord.com/api/webhooks/";
-// todo:
-// pub const DISCORD_EMBED_LIMIT: u8 = 10;
+pub const DEFAULT_EMBED_LIMIT: u8 = 10;
 
 pub fn get_env<T: FromStr>(name: &str, require: bool) -> Option<T> {
 	return match env::var(name) {
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod env;
 mod err;
 mod webhook;
 
+use async_recursion::async_recursion;
 use hyper::{
 	service::{make_service_fn, service_fn},
 	Body, Error, Request, Response, Server, StatusCode,
@@ -16,15 +17,15 @@ use std::{
 	time::{Duration, SystemTime, UNIX_EPOCH},
 };
 use tokio::{
-	sync::Mutex,
+	sync::{Mutex, MutexGuard},
 	task::{self, JoinHandle},
 	time::sleep,
 };
 
 use crate::{
-	env::{get_env, get_env_default, DEFAULT_DELIVER_DURATION, DEFAULT_PORT},
+	env::{get_env, get_env_default, DEFAULT_DELIVER_DURATION, DEFAULT_EMBED_LIMIT, DEFAULT_PORT},
 	err::form_error_res,
-	webhook::{parse_body, validate_request, WebhookBatch, WebhookParts},
+	webhook::{parse_body, validate_request, WebhookBatch, WebhookParts, WebhookPayload},
 };
 
 lazy_static! {
@@ -38,9 +39,91 @@ lazy_static! {
 
 		return DEFAULT_DELIVER_DURATION;
 	};
+	static ref EMBED_LIMIT: u8 = {
+		let limit: Option<u8> = get_env("EMBED_LIMIT", false);
+		if limit.is_some() {
+			return limit.unwrap();
+		}
+
+		return DEFAULT_EMBED_LIMIT;
+	};
 }
 
-async fn handle_request(request: Request<Body>) -> Result<Response<Body>, Error> {
+#[async_recursion]
+async fn handle_request(
+	payload: WebhookPayload,
+	webhook_parts: WebhookParts,
+	batch_map: &mut MutexGuard<HashMap<WebhookParts, WebhookBatch>>,
+	task_map: &mut MutexGuard<HashMap<WebhookParts, JoinHandle<()>>>,
+) -> Result<Response<Body>, Error> {
+	let task = task_map.entry(webhook_parts.clone());
+	if let Occupied(mut entry) = task {
+		let task = entry.get_mut();
+		task.abort();
+		entry.remove();
+		task_map.remove(&webhook_parts);
+	};
+
+	let batch_payload = payload.clone();
+	let batch = match batch_map.entry(webhook_parts.clone()) {
+		Occupied(entry) => {
+			let batch = entry.into_mut();
+			let embeds = batch
+				.payloads
+				.iter()
+				.filter(|payload| payload.embeds.is_some())
+				.flat_map(|payload| payload.embeds.as_ref().unwrap())
+				.collect::<Vec<_>>();
+			if embeds.len() >= *EMBED_LIMIT as usize {
+				webhook::deliver(batch.clone()).await;
+				batch_map.remove(&webhook_parts);
+				task_map.remove(&webhook_parts);
+				return handle_request(payload, webhook_parts, batch_map, task_map).await;
+			}
+
+			batch.payloads.push(batch_payload);
+			batch.clone()
+		}
+		Vacant(entry) => {
+			let key = entry.key().clone();
+			let batch = entry.insert(WebhookBatch {
+				created: SystemTime::now(),
+				payloads: vec![batch_payload],
+				parts: key,
+			});
+
+			batch.clone()
+		}
+	};
+
+	let since_epoch = batch.created.duration_since(UNIX_EPOCH).unwrap();
+	let response = Response::builder()
+		.status(StatusCode::NO_CONTENT)
+		.header(
+			"X-Batch-Id",
+			format!("{}-{}", batch.parts.webhook_id, batch.parts.webhook_token),
+		)
+		.header("X-Batch-Size", batch.payloads.len())
+		.header("X-Batch-Created", since_epoch.as_millis().to_string())
+		.body(Body::empty())
+		.unwrap();
+
+	let task_parts = webhook_parts.clone();
+	let task = task::spawn(async move {
+		sleep(*DELIVER_DURATION).await;
+		let mut task_map = TASK_MAP.lock().await;
+		let mut batch_map = BATCH_MAP.lock().await;
+
+		webhook::deliver(batch).await;
+		batch_map.remove(&task_parts);
+		task_map.remove(&task_parts);
+	});
+
+	task_map.insert(webhook_parts, task);
+	return Ok(response);
+}
+
+async fn forward_request(request: Request<Body>) -> Result<Response<Body>, Error> {
 	let (parts, body) = request.into_parts();
 	let webhook_parts = match validate_request(&parts) {
 		Ok(parts) => parts,
@@ -59,66 +142,23 @@ async fn handle_request(request: Request<Body>) -> Result<Response<Body>, Error>
 		}
 	};
 
-	let mut batch_map = BATCH_MAP.lock().await;
-	let batch = match batch_map.entry(webhook_parts.clone()) {
-		Occupied(entry) => {
-			let batch = entry.into_mut();
-			batch.payloads.push(payload);
-			batch.clone()
-		}
-		Vacant(entry) => {
-			let key = entry.key().clone();
-			let batch = entry.insert(WebhookBatch {
-				created: SystemTime::now(),
-				payloads: vec![payload],
-				parts: key,
-			});
-
-			batch.clone()
-		}
-	};
+	if payload.embeds.is_some() && payload.embeds.as_ref().unwrap().len() > *EMBED_LIMIT as usize {
+		// note: could be neat to queue up payloads and send them later - project for another day,
+		// could be relevant: https://doc.rust-lang.org/std/collections/struct.VecDeque.html
+		let response = form_error_res(StatusCode::BAD_REQUEST, 102, "Too many embeds");
+		return Ok(response);
+	}
 
 	let mut task_map = TASK_MAP.lock().await;
-	let task = task_map.entry(webhook_parts.clone());
-	if let Occupied(mut entry) = task {
-		let task = entry.get_mut();
-		task.abort();
-		entry.remove();
-		task_map.remove(&webhook_parts);
-	};
-
-	let task_batch = batch.clone();
-	let task_parts = webhook_parts.clone();
-	let task = task::spawn(async move {
-		sleep(*DELIVER_DURATION).await;
-		let mut task_map = TASK_MAP.lock().await;
-		let mut batch_map = BATCH_MAP.lock().await;
-
-		webhook::deliver(task_batch).await;
-		batch_map.remove(&task_parts);
-		task_map.remove(&task_parts);
-	});
-
-	task_map.insert(webhook_parts, task);
-	let since_epoch = batch.created.duration_since(UNIX_EPOCH).unwrap();
-	let response = Response::builder()
-		.status(StatusCode::NO_CONTENT)
-		.header(
-			"X-Batch-Id",
-			format!("{}-{}", batch.parts.webhook_id, batch.parts.webhook_token),
-		)
-		.header("X-Batch-Size", batch.payloads.len())
-		.header("X-Batch-Created", since_epoch.as_millis().to_string())
-		.body(Body::empty())
-		.unwrap();
-	return Ok(response);
+	let mut batch_map = BATCH_MAP.lock().await;
+	return handle_request(payload, webhook_parts, &mut batch_map, &mut task_map).await;
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let port: u16 = get_env_default("LISTEN_PORT", DEFAULT_PORT);
 	let addr = SocketAddr::from(([127, 0, 0, 1], port));
-	let make_svc = make_service_fn(|_conn| async { Ok::<_, Error>(service_fn(handle_request)) });
+	let make_svc = make_service_fn(|_conn| async { Ok::<_, Error>(service_fn(forward_request)) });
 	let server = Server::bind(&addr).serve(make_svc);
 	println!("Server listening at {}", server.local_addr());
 	server.await?;
